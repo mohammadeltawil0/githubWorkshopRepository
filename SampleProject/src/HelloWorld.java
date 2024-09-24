21d25abbd54d522636955e29c8c6cb5b27665f9f
Mohammad El-Tawil <mohammadeltawil0@gmail.com>
2024-09-24 13:20:41 -0400

    Initial commit

---
+++ SampleProject/src/HelloWorld.java
@@ -0,0 +1,11 @@
+/*
+ * This code is a sample for Github Workshop meeting for the leaders on 9/24/2024
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
